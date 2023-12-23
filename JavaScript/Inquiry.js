81ceb85a58e50652ca6cd9baaba7b669a8689072
LahiruHarshana <lharshana2002@gmail.com>
2023-12-24 01:46:58 +0530

    updated

---
+++ JavaScript/Inquiry.js
@@ -0,0 +1,21 @@
+document.getElementById('inquiryForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Simple form validation
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill in all fields before submitting.');
+        return;
+    }
+
+    // You can add additional logic here, such as sending the form data to a server
+
+    // Confirmation alert
+    alert('Inquiry submitted successfully!\n\nName: ' + name + '\nEmail: ' + email + '\nMessage: ' + message);
+
+    // Reset the form
+    this.reset();
+});
