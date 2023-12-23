9c70d94320e37821c0e26c687a929ece3a71fe25
LahiruHarshana <lharshana2002@gmail.com>
2023-12-24 01:33:09 +0530

    updated

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -5,10 +5,24 @@ function toggleMenu() {
 
 function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId);
-    var yOffset = -50;
+    var yOffset = -50; // Adjust as needed
     var y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
     window.scrollTo({ top: y, behavior: 'smooth' });
+
+    // Close the menu on small screens after clicking a link
     var menu = document.querySelector('nav ul');
     menu.classList.remove('show');
 }
+
+// JavaScript for changing header background on scroll
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
