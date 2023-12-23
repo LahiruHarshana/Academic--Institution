d658618a37dec262b8bce64b0dca51ad92d74ab2
LahiruHarshana <lharshana2002@gmail.com>
2023-12-24 01:27:52 +0530

    updated

--- .idea/JavaScript/Home.js
+++ .idea/JavaScript/Home.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     var menu = document.querySelector('nav ul');
     menu.classList.toggle('show');
 }
+
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    var yOffset = -50; // Adjust as needed
+    var y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: 'smooth' });
+    var menu = document.querySelector('nav ul');
+    menu.classList.remove('show');
+}
